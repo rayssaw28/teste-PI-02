146ced956cdca2f8da260621c2a11c76791f1f37
rayssaw28 <161396496+rayssaw28@users.noreply.github.com>
2025-11-26 13:21:18 -0300

    Delete greenLogSolution/greenLogSolution directory

--- greenLogSolution/greenLogSolution/src/main/java/com/greenLogBackend/greenLogSolution/enums/PerfilUsuario.java
+++
@@ -1,7 +0,0 @@
-package com.greenLogBackend.greenLogSolution.enums;
-
-public enum PerfilUsuario {
-    ROLE_ADMIN,
-    ROLE_MOTORISTA,
-    ROLE_GESTOR
-}
--- greenLogSolution/greenLogSolution/src/main/java/com/greenLogBackend/greenLogSolution/repository/RotaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greenLogBackend.greenLogSolution.repository;
-
-import com.greenLogBackend.greenLogSolution.entity.Rota;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RotaRepository extends JpaRepository<Rota, Long> {
-}
