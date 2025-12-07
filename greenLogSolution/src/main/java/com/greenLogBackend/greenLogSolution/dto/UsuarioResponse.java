01235313c82b7fcf401f646ff3c2b8fb114726b3
rayssaw28 <161396496+rayssaw28@users.noreply.github.com>
2025-12-07 11:48:28 -0300

    Delete greenLogSolution directory

--- greenLogSolution/src/main/java/com/greenLogBackend/greenLogSolution/dto/UsuarioResponse.java
+++
@@ -1,9 +0,0 @@
-package com.greenLogBackend.greenLogSolution.dto;
-
-import com.greenLogBackend.greenLogSolution.enums.PerfilUsuario;
-
-public record UsuarioResponse(
-        Long id,
-        String login,
-        PerfilUsuario perfil
-) {}
--- greenLogSolution/src/main/java/com/greenLogBackend/greenLogSolution/exception/BusinessException.java
+++
@@ -1,5 +0,0 @@
-package com.greenLogBackend.greenLogSolution.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) { super(message); }
-}
